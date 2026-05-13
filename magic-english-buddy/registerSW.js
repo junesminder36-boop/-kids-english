8f1c923840f052422ab036d8a93e08122b8c426a
蔡悦 <caiyue@caiyuedeMacBook-Air.local>
2026-05-13 21:56:32 +0800

    Update to Power Up curriculum with fixed audio; preserve Magic English Buddy sub-app

---
+++ magic-english-buddy/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/magic-english-buddy/sw.js', { scope: '/magic-english-buddy/' })})}
--- server.js
+++ server.js
@@ -6,10 +6,16 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname)));
 
+app.get('/magic-english-buddy/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'magic-english-buddy', 'index.html'));
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
   console.log(`Bobo English server running on port ${PORT}`);
+  console.log(`Main app: http://localhost:${PORT}/`);
+  console.log(`Magic English Buddy: http://localhost:${PORT}/magic-english-buddy/`);
 });
